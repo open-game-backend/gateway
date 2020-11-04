08b25fb58c2b5cd39af8bb04824b679513a97a60
Nick Prühs <dev@npruehs.de>
2020-11-04 22:56:26 +0100

    Add Elastic Common Schema logging and log application version at startup.

--- src/main/java/de/opengamebackend/gateway/GatewayApplication.java
+++ src/main/java/de/opengamebackend/gateway/GatewayApplication.java
@@ -1,11 +1,13 @@
 package de.opengamebackend.gateway;
 
+import de.opengamebackend.util.EnableOpenGameBackendUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringBootApplication
+@EnableZuulProxy
+@EnableOpenGameBackendUtils
 public class GatewayApplication {
 	public static void main(String[] args) {
 		// https://www.baeldung.com/zuul-load-balancing
