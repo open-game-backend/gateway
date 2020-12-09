f27938dafae7c7d850dc6335b05f28b00d4c22b8
Nick Prühs <dev@npruehs.de>
2020-12-09 17:52:28 +0100

    Enable validation for configuration.

--- src/main/java/de/opengamebackend/gateway/GatewayConfig.java
+++ src/main/java/de/opengamebackend/gateway/GatewayConfig.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotNull;
 
 @ConstructorBinding
 @ConfigurationProperties("de.opengamebackend.gateway")
+@Validated
 public class GatewayConfig {
     private static final long TOKEN_EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
